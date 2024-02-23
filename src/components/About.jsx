f4719dce1b3fdd5731ec882cfe4fd1f23194b573
yannick-98 <yannick9862@gmail.com>
2024-02-23 01:24:16 +0100

    ajustes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -31,7 +31,7 @@ const About = () => {
                 <p className=''>About me:</p>
                 <div className='space-y-4 p-4 bg-black bg-opacity-45 rounded-lg text-xl'>
                     <p>I learned web development self-taught, and now I continue my education at the Next Business School,
-                        pursuing a master's degree in Big Data and Business Intelligence. </p>
+                        pursuing an online master's degree in Big Data and Business Intelligence. </p>
                     <p> I love challenges, and one of them was working in the Netherlands and Belgium, where I learned
                         to navigate in English, in addition to discovering my passion for exploring other cultures.</p>
                     <p>I enjoy road trips, boxing, and algorithmic trading, which has led me to the exciting project I
@@ -69,6 +69,10 @@ const About = () => {
                     <section className='space-y-1 w-1/2 sm:w-1/4 pb-10'>
                         <h1 className='text-center text-sm pb-3 underline'>Backend</h1>
                         <section className='flex  items-center gap-1'>
+                            <img src={JsIcon} alt="" className='w-5' />
+                            <p>Javascript </p>
+                        </section>
+                        <section className='flex  items-center gap-1'>
                             <img src={NodeIcon} alt="" className='w-5' />
                             <p>Node.js </p>
                         </section>
@@ -77,24 +81,24 @@ const About = () => {
                             <p>Python </p>
                         </section>
                         <section className='flex  items-center gap-1'>
-                            <img src={PyNodeIcon} alt="" className='w-5' />
-                            <p>PyNode </p>
-                        </section>
-                        <section className='flex  items-center gap-1'>
                             <img src={MongodbIcon} alt="" className='w-5' />
                             <p>MongoDB </p>
                         </section>
                     </section>
 
                     <section className='space-y-1 w-1/2 sm:w-1/4 pb-10'>
-                        <h1 className='text-center text-sm pb-3 underline'>Design</h1>
+                        <h1 className='text-center text-sm pb-3 underline'>Others</h1>
                         <section className='flex  items-center gap-1'>
                             <img src={FigmaIcon} alt="" className='w-5' />
-                            <p>Figma </p>
+                            <p>Git </p>
                         </section>
                         <section className='flex  items-center gap-1'>
                             <img src={PyNodeIcon} alt="" className='w-5' />
-                            <p>Adobe </p>
+                            <p>GitHub </p>
+                        </section>
+                        <section className='flex  items-center gap-1'>
+                            <img src={FigmaIcon} alt="" className='w-5' />
+                            <p>Figma </p>
                         </section>
                     </section>
 
--- src/components/Kaly.jsx
+++ src/components/Kaly.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import Kalyimg from '../assets/Kaly.png'
 
 const Kaly = () => {
     return (
@@ -10,9 +11,9 @@ const Kaly = () => {
                 </NavLink>
                 <p className='text-2xl pb-4'>Kaly</p>
                 <div className='md:flex md:gap-3'>
-                    <img src={''} alt="" className='rounded w-full max-w-[600px] mx-auto ' />
+                    <img src={Kalyimg} alt="" className='rounded w-full max-w-[600px] mx-auto ' />
                     <div className='flex flex-col justify-between'>
-                        <div className='space-y-3'>
+                        <div className='space-y-3 text-xl pt-4'>
                             <p className='text-sm text-slate-300'>Frontend: </p>
                             <p>HTML, CSS, JavaScript, React, Tailwindcss</p>
                             <p className='text-sm text-slate-300'>Backend: </p>
@@ -20,15 +21,14 @@ const Kaly = () => {
                             <p className='text-sm text-slate-300'>Database: </p>
                             <p>MongoDB</p>
                         </div>
-                        <NavLink className='mt-12 underline text-sm text-center '>Go to website</NavLink>
+                        <NavLink className='mt-12 bg-orange-700 border-2 border-black rounded-lg mx-auto p-2 text-sm text-center '>Go to website</NavLink>
                     </div>
                 </div>
             </section>
-            <section className='space-y-4'>
-                <p className='text-lg'>The goal of this project is to create a website that allows users to save time
-                    analyzing market data. The website will provide real-time and historical data
-                    from a variety of sources, as well as tools for automated tracking.</p>
-                <p></p>
+            <section className='space-y-4 text-xl'>
+                <p>This website is a personal organizer that allows you to plan events, tasks, and daily notes.</p>
+                <p>This project's frontend is hosted on Netlify, while the backend is hosted on Render and
+                    the database uses MongoDB.</p>
             </section>
         </div>
     )
